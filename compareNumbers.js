8a0a9d324a78490b3a583462a2c6bcc88d7ad073
sethripman <sethripman@gmail.com>
2019-10-01 17:08:30 -0700

    added test cases and compareNumbers method

---
+++ compareNumbers.js
@@ -0,0 +1,5 @@
+export const compareNumbers = (guess, correctNumber) => {
+    if (guess === correctNumber) return 0;
+    if (guess < correctNumber) return -1;
+    if (guess > correctNumber) return 1;
+};
